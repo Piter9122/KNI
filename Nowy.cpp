2e4ce2ea554795b433b27106f5efaa1829547961
Piter9122 <piter9122@gmail.com>
2014-03-20 18:01:01 +0100

    Komentarzyk

--- Nowy.cpp
+++ Nowy.cpp
@@ -2,10 +2,23 @@
 
 using namespace std;
 
+#define N 10
 #define MEJN int main(){
-	
+#define RITERN return 0;}	
+#define PENTLA for(int i = 0; i < N; ++i){
+#define KONIEC_PENTLI }
+
+#define WARUNECZEK(x) if(x){
+#define RANDOMIK rand()%2
+#define LUB_NIE
+
 MEJN
-
-
-	return 0;
-}
+	PENTLA
+	
+		WARUNECZEK(RANDOMIK)
+			cout << ":-)" << endl;
+		ALBO_NIE
+			cout << ":-(" << endl;
+	
+	KONIEC_PENTLI
+RITERN
